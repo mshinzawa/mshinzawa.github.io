4f868eac9293a64bfe67ea642690c669df0242d7
Momo Shinzawa <mshinzawa@gmail.com>
2021-09-19 13:26:25 -0400

    your first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# mshinzawa.github.io
---
+++ js/script.js
@@ -0,0 +1,20 @@
+// Assign the modal
+
+var modal = document.getElementById("donationModal");
+
+// Assign the button to open the hidden donation box
+var btn = document.getElementById("donation-btn");
+
+// When user click the donate now button, it will close the modal
+var span = document.getElementsByClassName("donation_close_button")[0];
+
+// when the user click the "Make a Gift" button, open the modal
+btn.onclick = function() {
+  modal.style.display="block";
+}
+
+// When the user click on "donate now" button, it will close the modal.
+span.onclick = function() {
+  modal.style.display="none";
+}
+
